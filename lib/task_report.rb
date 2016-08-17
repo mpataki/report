1c5fdaf50c88cf04922c354636339d1190c3a458
Mat Pataki <matpataki@gmail.com>
2016-08-17 19:33:10 +0000

    move retry logic out of executable and allow for report reuse between commands if it's the same session (as in, a retry)

--- lib/task_report.rb
+++ lib/task_report.rb
@@ -6,67 +6,70 @@ require 'task_report/report'
 module TaskReport
   class << self
     def start(new_task_description)
-      report =
+      @report ||=
         if report_gist.nil?
           Report.create(new_task_description: new_task_description)
         else
           Report.create_from_gist(report_gist).tap(&:stop_all_tasks)
         end
 
-      report.start_task(new_task_description)
-      report.save_to_gist!
+      @report.start_task(new_task_description)
+      @report.save_to_gist!
+    rescue Report::TaskAlreadyTracked
+    	puts "Task '#{new_task_description}' is already being tracked. Continuing the task."
+    	continue(new_task_description)
     end
 
     def stop
       return if no_gist?
 
-      report = Report.create_from_gist(report_gist)
-      report.stop_all_tasks
-      report.save_to_gist!
+      @report ||= Report.create_from_gist(report_gist)
+      @report.stop_all_tasks
+      @report.save_to_gist!
       puts "All tasks stopped"
     end
 
     def continue(task_id)
       return if no_gist?
 
-      report = Report.create_from_gist(report_gist)
-      report.continue(task_id)
-      report.save_to_gist!
+      @report ||= Report.create_from_gist(report_gist)
+      @report.continue(task_id)
+      @report.save_to_gist!
     rescue Report::TaskAlreadyOngoing, Task::TaskOngoing => e
       puts "Task already underway - #{e.message}"
     end
 
     def list
       return if no_gist?
-      Report.create_from_gist(report_gist).print_tasks
+      (@report || Report.create_from_gist(report_gist)).print_tasks
     end
 
     def delete(identifier)
       return if no_gist?
 
-      report = Report.create_from_gist(report_gist)
+      @report ||= Report.create_from_gist(report_gist)
 
       case identifier
-      when 'all'
-        report.delete_all
+      when 'today'
+        @report.delete_all
       when 'gist'
         puts "Deleting today's report gist"
-        Gist.delete(report.gist_id)
+        Gist.delete(@report.gist_id)
         return
       else
-        report.delete(identifier)
+        @report.delete(identifier)
       end
 
-      report.save_to_gist!
+      @report.save_to_gist!
     rescue Report::TaskDNE
-      puts "Task '#{task_id}' does not exist - nothing to do."
+      puts "Task '#{identifier}' does not exist - nothing to do."
     end
 
     def current
       return if no_gist?
 
-      report = Report.create_from_gist(report_gist)
-      report.print_current_task
+      @report ||= Report.create_from_gist(report_gist)
+      @report.print_current_task
     rescue Report::MultipleOngoingTasks
       puts 'Something went wrong. There are multiple ongoing tasks.'
     end
